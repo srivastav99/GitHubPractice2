273447f7d28595f4b2326204f889b351125d8702
Byna Srivastav <sribyna99@gmail.com>
2023-10-18 15:07:28 +0530

    SubtractTwoNumbers class added

---
+++ githubPractice2TL/src/main/java/githubPractice2TL/SubtractTwoNumbers.java
@@ -0,0 +1,13 @@
+package githubPractice2TL;
+
+public class SubtractTwoNumbers {
+
+	public static void main(String[] args) {
+		
+		int a=20,b=10,diff;
+		diff=a-b;
+		System.out.println("Subtraction Of two number "+a+" and "+b+" is "+diff);
+
+	}
+
+}
