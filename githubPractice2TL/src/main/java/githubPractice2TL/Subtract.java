b2db0bdbd948a558a0cf8b2b6c4e0dec4c50cce0
qaShoaib <qashoaibakhtar@gmail.com>
2023-10-18 21:34:04 +0530

    New Class is Add

--- githubPractice2TL/src/main/java/githubPractice2TL/Subtract.java
+++ githubPractice2TL/src/main/java/githubPractice2TL/Subtract.java
@@ -4,7 +4,7 @@ public class Subtract {
 
 	public static void main(String[] args) {
 		
-		int a=100,b=50,diff;
+		int a=100,b=10,diff;
 		diff=a-b;
 		System.out.println("Subtraction Of two number "+a+" and "+b+" is "+diff);
 
